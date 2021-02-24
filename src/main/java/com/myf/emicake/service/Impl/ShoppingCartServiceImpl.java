0308d90fc12ec41b7f3c3dd10d30a9646c7ebc98
afeng <emiriatani@qq.com>
2021-02-24 21:48:27 +0800

    2021-02-24

---
+++ src/main/java/com/myf/emicake/service/Impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,13 @@
+package com.myf.emicake.service.Impl;
+
+import com.myf.emicake.service.ShoppingCartService;
+
+/**
+ * @ClassName com.myf.emicake.service.Impl ShoppingCartServiceImpl
+ * @Description
+ * @Author Afengis
+ * @Date 2021/2/24 19:00
+ * @Version V1.0
+ **/
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+}
---
+++ src/main/java/com/myf/emicake/service/ShoppingCartService.java
@@ -0,0 +1,14 @@
+package com.myf.emicake.service;
+
+/**
+ * @InterfaceName com.myf.emicake.service ShoppingCartService
+ * @Description
+ * @Author Afengis
+ * @Date 2021/2/24 18:59
+ * @Version V1.0
+ **/
+public interface ShoppingCartService {
+
+
+
+}
