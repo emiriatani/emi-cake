2922d1ee3ae013f8c6e6a8c79f49afd8fb2ecd11
afeng <emiriatani@qq.com>
2021-03-02 23:06:35 +0800

    2021-03-02

--- src/main/resources/static/assets/js/cart.js
+++ src/main/resources/static/assets/js/cart.js
@@ -8,57 +8,7 @@ var cartItem = document.getElementsByClassName("cart_item");
 var removeItem = document.getElementsByClassName("remove_item");
 
 
-/*清空购物车*/
-$("#deleteAll").click(function () {
-    layui.use(['layer'], function (e) {
-        var layer = layui.layer;
-        var msg = "确定要清空购物车吗？";
-        layer.ready(function (e) {
-            layer.msg(msg, {
-                btn: ['确定', '取消']
-                , time: 10 * 60 * 1000
-                , yes: function (e) {
-                    /*删除确定按钮回调*/
-                }
-                , btn2: function () {
-                    /*删除取消按钮回调*/
-                    layer.close();
-                }
-            });
-        });
-    });
-})
 
-
-/*删除商品*/
-$(".remove_item").click(function (e) {
-    layui.use(['layer'], function (e) {
-        var layer = layui.layer;
-        var msg = "确定要删除吗？";
-        layer.ready(function (e) {
-            layer.msg(msg, {
-                btn: ['确定', '取消']
-                , time: 10 * 60 * 1000
-                , yes: function (e) {
-                    /*删除确定按钮回调*/
-                }
-                , btn2: function () {
-                    /*删除取消按钮回调*/
-                    layer.close();
-                }
-            });
-        });
-    });
-})
-
-/*购物车总金额价格计算*/
-function sum() {
-    var sum = 0;
-    for (var i = 0; i < itemPrice.length; i++) {
-        sum += parseInt(itemPrice[i].innerText)
-    }
-    totalPrice.innerHTML = sum;
-}
 
 /*增加商品数量事件*/
 for (var i = 0, len = add.length; i < len; i++) {
@@ -97,3 +47,58 @@ for (var i = 0, len = del.length; i < len; i++) {
         }
     })(i)
 }
+
+/*购物车总金额价格计算*/
+function sum() {
+    var sum = 0;
+    for (var i = 0; i < itemPrice.length; i++) {
+        sum += parseInt(itemPrice[i].innerText)
+    }
+    totalPrice.innerHTML = sum;
+}
+
+/*清空购物车*/
+$("#deleteAll").click(function () {
+    layui.use(['layer'], function (e) {
+        var layer = layui.layer;
+        var msg = "确定要清空购物车吗？";
+        layer.ready(function (e) {
+            layer.msg(msg, {
+                btn: ['确定', '取消']
+                , time: 10 * 60 * 1000
+                , yes: function (e) {
+                    /*删除确定按钮回调*/
+                }
+                , btn2: function () {
+                    /*删除取消按钮回调*/
+                    layer.close();
+                }
+            });
+        });
+    });
+})
+
+/*删除商品*/
+$(".remove_item").click(function (e) {
+    layui.use(['layer'], function (e) {
+        var layer = layui.layer;
+        var msg = "确定要删除吗？";
+        layer.ready(function (e) {
+            layer.msg(msg, {
+                btn: ['确定', '取消']
+                , time: 10 * 60 * 1000
+                , yes: function (e) {
+                    /*删除确定按钮回调*/
+                }
+                , btn2: function () {
+                    /*删除取消按钮回调*/
+                    layer.close();
+                }
+            });
+        });
+    });
+})
+
+
+
+
